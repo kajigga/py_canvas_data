f75a40c277ce959f36c4ac0f1f9309f57fc8204a
K Developer <kajigga@gmail.com>
2016-04-14 10:52:01 -0600

    modified docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('../'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
-    'sphinx.ext.githubpages',
+    #'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
